176bc89a73324008dce421ea30aa29ebebf392b3
AfenMarbun <114843383+AfenMarbun@users.noreply.github.com>
2024-02-28 09:21:28 +0700

    Update 122140155_Rustian Afencius Marbun_Pset(3).py

--- week0-pbo-rc/122140155_Rustian Afencius Marbun_Pset(3).py
+++ week0-pbo-rc/122140155_Rustian Afencius Marbun_Pset(3).py
@@ -1,1 +1,10 @@
-test
+biodata = '''Nama : Rustian Afencius Marbun
+NIM : 122140155
+Resolusi Saya di Tahun ini : Upgrade programming skill'''
+
+with open("Me.txt", "w") as file:
+    file.write(biodata)
+
+with open("Me.txt", "r") as file:
+    content = file.read()
+    print(content)
